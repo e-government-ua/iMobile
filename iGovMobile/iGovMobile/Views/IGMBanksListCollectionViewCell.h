5d0fbde11a7eb83010e1b645aac80d48d74bfd0f
Dima Shevchuk <dmytroshevchuk@MacBook-Pro-Dmytro.local>
2015-11-04 23:30:10 +0200

    Add Select Bank screen

---
+++ iGovMobile/iGovMobile/Views/IGMBanksListCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  IGMBanksListCollectionViewCell.h
+//  iGovMobile
+//
+//  Created by Dmytro Shevchuk on 04.11.15.
+//  Copyright © 2015 iGov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGMBanksListCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@end
