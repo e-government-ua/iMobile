1b940126ac8ca4f99ddfe2a84b95ef6714c7a3e4
Dima Shevchuk <dmytroshevchuk@MacBook-Pro-Dmytro.local>
2015-11-04 23:29:17 +0200

    Create Project

---
+++ iGovMobile/iGovMobile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iGovMobile
+//
+//  Created by Dmytro Shevchuk on 04.11.15.
+//  Copyright © 2015 iGov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
