5bf2c8a26ab7cc43b43cfd0ef5eb9fa2f31bb73b
Yurii Kolesnykov <yurikoles@gmail.com>
2016-07-03 16:24:41 +0300

    причесан

--- iGov/Networking.swift
+++ iGov/Networking.swift
@@ -6,25 +6,16 @@
 //  Copyright © 2016 iGov. All rights reserved.
 //
 
-import Foundation
+
 import Alamofire
 import SwiftyJSON
 
 class Networking
 {
     static let sharedInstance = Networking()
-    
-    
     func getListOfServices(completion: (([Category])->Void)? ) {
-      
-        
         Alamofire.request(.GET, "https://igov.org.ua/api/catalog?bShowEmptyFolders=false")
             .responseJSON { response in
-                print(response.request)  // original URL request
-                print(response.response) // URL response
-                print(response.data)     // server data
-                print(response.result)   // result of response serialization
-                
                 if let value = response.result.value {
                     let json = JSON(value)
                     if let categories = json.array {
--- iGov/ViewController.swift
+++ iGov/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         Networking.sharedInstance.getListOfServices { (categories: [Category]) in
+            print (categories)
         }
     }
 
