146c9b4cde526c35dbea52f27d5d4f30aae4980d
sergs1207 <sergs1207@gmail.com>
2017-02-23 15:20:24 +0200

    Initial implementation of Login screen (#15)

---
+++ iGov/ButtonTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  ButtonTableViewCell.swift
+//  iGov
+//
+//  Created by Sergii Maksiuta on 2/23/17.
+//  Copyright © 2017 iGov. All rights reserved.
+//
+
+import UIKit
+
+class ButtonTableViewCell: UITableViewCell {
+
+    lazy var button = UIButton()
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.addSubview(button)
+        button.snp.makeConstraints { (make) in
+            make.edges.equalTo(self).inset(UIEdgeInsets.init(top: 5, left: 20, bottom: 5, right: 20))
+        }
+        button.setTitle("Безпечний вхід", for: .normal)
+        button.backgroundColor = UIColor(red: 81/255.0, green: 85/255.0, blue: 93/255.0, alpha: 1)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    class func reuseIdentifier() -> String{
+        return "ButtonTableViewCell"
+    }
+
+}
---
+++ iGov/LoginTableViewController.swift
@@ -0,0 +1,103 @@
+//
+//  LoginTableViewController.swift
+//  iGov
+//
+//  Created by Sergii Maksiuta on 2/23/17.
+//  Copyright © 2017 iGov. All rights reserved.
+//
+
+import UIKit
+
+class LoginTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.register(BankSelectorTableViewCell.self,
+                                forCellReuseIdentifier:BankSelectorTableViewCell.reuseIdentifier())
+        self.tableView.register(TextFieldTableViewCell.self,
+                                forCellReuseIdentifier:TextFieldTableViewCell.reuseIdentifier())
+        self.tableView.register(ButtonTableViewCell.self,
+                                forCellReuseIdentifier:ButtonTableViewCell.reuseIdentifier())
+        
+        //Here we should register new cell
+//        self.tableView.style = .grouped
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 3
+        default:
+            return 0
+        }
+    }
+    
+    
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if (section == 0){
+            return 50
+        }
+        return 0
+    }
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if (section == 0){
+            let result = UILabel()
+            result.text = "Щоб увійти до системи BankID,\n треба увійти у свій банк."
+            result.numberOfLines = 0
+            result.font = UIFont(name: "SFUIText-Regular", size: 15)
+            result.textAlignment = .center
+            
+            return result
+        }
+        return nil
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if (indexPath.section < 1){
+            return 44
+        }
+        return 50
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: BankSelectorTableViewCell.reuseIdentifier(), for: indexPath)
+            cell.textLabel?.text = "Privatbank"
+            cell.accessoryType = .disclosureIndicator
+            return cell
+        case 1:
+            if (indexPath.row < 2)
+            {
+                let cell = tableView.dequeueReusableCell(withIdentifier: TextFieldTableViewCell.reuseIdentifier(), for: indexPath)
+                return cell
+            }
+            else {
+                let cell = tableView.dequeueReusableCell(withIdentifier: ButtonTableViewCell.reuseIdentifier(), for: indexPath)
+                return cell
+            }
+        default:
+            return tableView.dequeueReusableCell(withIdentifier: TextFieldTableViewCell.reuseIdentifier(), for: indexPath)
+        }
+        
+    }
+    
+}
